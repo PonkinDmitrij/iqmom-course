8f262d4a6b22f2d0e642579861c5a141c5d856ec
Dmitrij Ponkin <ponkindmitrij@gmail.com>
2020-09-30 11:09:24 +0300

    Add smooth scrolling

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,8 @@
 import '../scss/style.scss';
+
+const arrToBottom = document.querySelector('.banner__to-bottom');
+const targetId = document.querySelector('#promo');
+
+arrToBottom.addEventListener('click', () => {
+  targetId.scrollIntoView({ behavior: 'smooth', block: 'start' });
+});
