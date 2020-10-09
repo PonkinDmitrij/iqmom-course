2ef779ac10a263b6641ed4656c530a8913610982
Dmitrij Ponkin <ponkindmitrij@gmail.com>
2020-10-09 09:52:24 +0300

    Add favicon

--- webpack.config.js
+++ webpack.config.js
@@ -52,6 +52,10 @@ module.exports = {
           from: path.resolve(__dirname, 'src/fonts'),
           to: path.resolve(__dirname, 'dist/fonts'),
         },
+        {
+          from: path.resolve(__dirname, 'src/favicon'),
+          to: path.resolve(__dirname, 'dist/img/favicon'),
+        },
       ],
     }),
     new MiniCssExtractPlugin({
